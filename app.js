ae72a932cd23e1b089556154ada448226768bab6
Chris Richter <wcrichter@gmail.com>
2017-02-24 16:34:25 -0500

    Added filter to GET /medications

--- app.js
+++ app.js
@@ -8,15 +8,29 @@ const {
     listMedsByForm,
     getMed
 } = require('./dal.js')
+const {
+    split
+} = require('ramda')
 const HTTPError = require('node-http-error')
 const port = process.env.PORT || 8080
 
 app.get('/medications', function(req, res, next) {
-    listMedsByLabel(function(err, meds) {
-        if (err) return next(new HTTPError(err.status, err.message, err))
-        res.status(200).send(meds)
-    })
+    if (req.query.filter && split(':', req.query.filter)[0] === 'ingredient') {
+        const result = split(':', req.query.filter)
+        listMedsByIngredient(result[1], function(err, meds) {
+            if (err) return next(new HTTPError(err.status, err.message, err))
+            res.status(200).send(meds)
+        })
+    } else if (!req.query.filter) {
+        listMedsByLabel(function(err, meds) {
+            if (err) return next(new HTTPError(err.status, err.message, err))
+            res.status(200).send(meds)
+        })
+    } else {
+      res.status(200).send([])
+    }
 })
+
 
 
 
@@ -26,11 +40,11 @@ app.get('/medications', function(req, res, next) {
 
 ///////////// error handler /////////////////////////////
 app.use(function(err, req, res, next) {
-  console.log(req.method, " ", req.path, "error:  ", err)
-  res.status(err.status || 500)
-  res.send(err)
+    console.log(req.method, " ", req.path, "error:  ", err)
+    res.status(err.status || 500)
+    res.send(err)
 })
 
 app.listen(port, function() {
-  console.log("I'm listening on port ", port)
+    console.log("I'm listening on port ", port)
 })
