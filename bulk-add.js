1dc51e2e0903aaeda8a4c488689b6b9a080efb3f
tripott <tripott@gmail.com>
2017-03-01 16:01:11 -0500

    moving bulk add scripts into one doc: bulk-add.js

--- bulk-add.js
+++ bulk-add.js
@@ -1,6 +1,6 @@
 const PouchDB = require('pouchdb-http')
 PouchDB.plugin(require('pouchdb-mapreduce'))
-const couch_base_uri = "http://127.0.0.1:3000/"
+const couch_base_uri = "http://127.0.0.1:5984/"
 const couch_dbname = "pharma-student" //remember pharmacy for me
 const db = new PouchDB(couch_base_uri + couch_dbname)
 
@@ -129,7 +129,7 @@ const docs = [{
         },
         "language": "javascript"
     },
-    
+
     {
         "_id": "_design/medsByLabel",
         "views": {
